df70ed3282441d28b3fe3e7467dd543f9187f620
Ermondel <ermondel@gmail.com>
2019-09-22 23:20:30 +0300

    feat: determine the current season and show its icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
-const SeasonDisplay = () => <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: "It's summer now",
+    iconName: 'sun'
+  },
+  winter: {
+    text: "It's winter now",
+    iconName: 'snowflake'
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={'massive ' + iconName + ' icon'} />
+      <h1>{text}</h1>
+      <i className={'massive ' + iconName + ' icon'} />
+    </div>
+  );
+};
 
 export default SeasonDisplay;
