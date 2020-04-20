dbd5a70b93208b26347ff95d15677c70d304652c
Ermondel <ermondel@gmail.com>
2020-04-20 14:50:25 +0300

    feat: start of the extended version

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,5 @@
 import React from 'react';
-import Info from './Info';
-import useLocation from './useLocation';
-import './App.css';
-import logo from '../logo.png';
 
-const App = () => {
-  const [coords, errorMessage] = useLocation();
-
-  let content;
-
-  if (errorMessage) {
-    content = <p className="error">{errorMessage}</p>;
-  } else if (coords) {
-    content = <Info coords={coords} />;
-  } else {
-    content = <p className="request">Please accept location request</p>;
-  }
-
-  return (
-    <div className="app">
-      <header className="header">
-        <img src={logo} alt="Compass" className="logo" />
-      </header>
-      {content}
-    </div>
-  );
-};
+const App = () => <div>under construction</div>;
 
 export default App;
