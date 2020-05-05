e3a177797aaa75f6a8d1240115ee0025ede62d80
ermondel <ermondel@gmail.com>
2020-05-05 17:08:09 +0300

    refactor: podcasts component (data source and list item)

--- src/components/Podcasts/PodcastList.js
+++ src/components/Podcasts/PodcastList.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectPodcast } from '../../actions/podcast';
-import podcasts from '../../fixtures/podcasts';
+import PodcastsItem from './PodcastsItem';
+import podcasts from './podcastsSource';
 
 const PodcastList = (props) => (
-  <div className="podcast-list">
+  <div className='podcast-list'>
     {podcasts.map((podcast) => (
-      <div className="podcast-list__item" key={podcast.id}>
-        <div className="podcast-list__content">
-          <h3 className="podcast-list__title">{podcast.title}</h3>
-          <p className="podcast-list__duration">{podcast.duration}</p>
-          <p className="podcast-list__author">{podcast.author}</p>
-        </div>
-        <div className="podcast-list__btns">
-          <button
-            className="podcast-list__select-btn"
-            onClick={() => props.selectPodcast(podcast)}
-          >
-            Select
-          </button>
-        </div>
-      </div>
+      <PodcastsItem
+        podcast={podcast}
+        onClickHandler={() => props.selectPodcast(podcast)}
+        key={podcast.id}
+      />
     ))}
   </div>
 );
---
+++ src/components/Podcasts/PodcastsItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PodcastsItem = ({ podcast, onClickHandler }) => (
+  <div className='podcast-list__item'>
+    <div className='podcast-list__content'>
+      <h3 className='podcast-list__title'>{podcast.title}</h3>
+      <p className='podcast-list__duration'>{podcast.duration}</p>
+      <p className='podcast-list__author'>{podcast.author}</p>
+    </div>
+    <div className='podcast-list__btns'>
+      <button className='podcast-list__select-btn' onClick={onClickHandler}>
+        Select
+      </button>
+    </div>
+  </div>
+);
+
+export default PodcastsItem;
