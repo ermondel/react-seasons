48c8aaa646be52b2c8f979d935543f78b6e41869
ermondel <ermondel@gmail.com>
2020-05-28 19:47:48 +0300

    feat: add sparklines for the weather component

--- src/components/Weather/WeatherItem.js
+++ src/components/Weather/WeatherItem.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 import { forecastsDelete } from '../../actions/weather';
 
-const WeatherItem = ({ forecast, forecastsDelete }) => (
-  <div className='weather-item'>
-    <div>{forecast.city.name}</div>
-    <div>{forecast.list[0].weather[0].main}</div>
-    <div>{forecast.list[0].weather[0].description}</div>
-    <div>
-      <button onClick={() => forecastsDelete(forecast.city.id)}>del</button>
+const WeatherItem = ({ forecast, forecastsDelete }) => {
+  const temperatureList = [];
+  const pressureList = [];
+  const humidityList = [];
+
+  forecast.list.forEach((weather) => {
+    temperatureList.push(weather.main.temp);
+    pressureList.push(weather.main.pressure);
+    humidityList.push(weather.main.humidity);
+  });
+
+  return (
+    <div className='weather-item'>
+      <div className='city-name'>{forecast.city.name}</div>
+      <div className='temperature-list'>
+        <Sparklines data={temperatureList}>
+          <SparklinesLine color='#ffffff' />
+        </Sparklines>
+      </div>
+      <div className='pressure-list'>
+        <Sparklines data={pressureList}>
+          <SparklinesLine color='#ffffff' />
+        </Sparklines>
+      </div>
+      <div className='humidity-list'>
+        <Sparklines data={humidityList}>
+          <SparklinesLine color='#ffffff' />
+        </Sparklines>
+      </div>
+      <div className='weather-btn'>
+        <button onClick={() => forecastsDelete(forecast.city.id)}>del</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default connect(undefined, { forecastsDelete })(WeatherItem);
