4e3edca0bf631cefd2637d6630874201ff146aa1
ermondel <ermondel@gmail.com>
2020-06-21 22:19:49 +0300

    feat: add form to create new posts

--- src/sections/Posts/comp/Add/AddContent.js
+++ src/sections/Posts/comp/Add/AddContent.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PostsAddForm from './PostsAddForm';
 
 const AddContent = () => (
   <div className='content'>
     <div className='content-wrap'>
       <h2>New post</h2>
+
+      <PostsAddForm />
     </div>
   </div>
 );
---
+++ src/sections/Posts/comp/Add/PostsAddForm.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsAddForm extends Component {
+  static maxLength = {
+    title: 80,
+    categories: 80,
+    content: 450,
+  };
+
+  static validate({ title, categories, content }) {
+    const errors = {};
+
+    // title
+    if (!title) {
+      errors.title = 'You must enter a title.';
+    }
+    if (title && title.length > PostsAddForm.maxLength.title) {
+      errors.title = `Must be ${PostsAddForm.maxLength.title} characters or less`;
+    }
+
+    // categories
+    if (!categories) {
+      errors.categories = 'You must enter categories.';
+    }
+    if (categories && categories.length > PostsAddForm.maxLength.categories) {
+      errors.categories = `Must be ${PostsAddForm.maxLength.categories} characters or less`;
+    }
+
+    // content
+    if (!content) {
+      errors.content = 'You must enter content.';
+    }
+    if (content && content.length > PostsAddForm.maxLength.content) {
+      errors.content = `Must be ${PostsAddForm.maxLength.content} characters or less`;
+    }
+
+    return errors;
+  }
+
+  renderError = ({ touched, error }) => {
+    if (touched && error) {
+      return <p>{error}</p>;
+    } else {
+      return null;
+    }
+  };
+
+  renderCounter = ({ name, value }) => {
+    if (value) {
+      return (
+        <span>
+          {value.length}/{PostsAddForm.maxLength[name]}
+        </span>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  renderField = ({ input, meta, placeholder, type }) => {
+    let el, counter, error;
+
+    switch (type) {
+      case 'text':
+        el = <input {...input} autoComplete='off' placeholder={placeholder} />;
+        break;
+
+      case 'textarea':
+        el = <textarea {...input} placeholder={placeholder}></textarea>;
+        break;
+
+      default:
+        el = null;
+        break;
+    }
+
+    counter = this.renderCounter(input);
+    error = this.renderError(meta);
+
+    return (
+      <div>
+        <div>
+          <label>{el}</label>
+          {counter}
+        </div>
+        {error}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    const { handleSubmit, valid } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <Field
+          name='title'
+          component={this.renderField}
+          placeholder='Title'
+          type='text'
+        />
+        <Field
+          name='categories'
+          component={this.renderField}
+          placeholder='Categories'
+          type='text'
+        />
+        <Field
+          name='content'
+          component={this.renderField}
+          placeholder='Content'
+          type='textarea'
+        />
+        <button disabled={!valid}>Save</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsAddForm',
+  validate: PostsAddForm.validate,
+})(PostsAddForm);
