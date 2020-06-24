a28037f45cf51ffb4e78503b4a9ee3c1080c0732
ermondel <ermondel@gmail.com>
2020-06-24 19:27:47 +0300

    refactor: modal window

--- src/app/ModalWindow/actions/ModalWindow.js
+++ src/app/ModalWindow/actions/ModalWindow.js
@@ -1,5 +1,3 @@
 import { MODAL_CLOSE } from '../../../types';
 
-export const modalClose = () => ({
-  type: MODAL_CLOSE,
-});
+export const modalClose = () => ({ type: MODAL_CLOSE });
--- src/app/ModalWindow/comp/ModalWindow.js
+++ src/app/ModalWindow/comp/ModalWindow.js
@@ -2,26 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const ModalWindow = ({ visible, onWindowClose, children }) => {
-  let content = (
+  const stopPropagation = (event) => event.stopPropagation();
+
+  const button = (
+    <button
+      className='modal__btn-close'
+      onClick={onWindowClose}
+      title='Close window'
+    >
+      <span>Close window</span>
+    </button>
+  );
+
+  const content = (
     <div className={`modal ${!visible ? 'modal--hide' : ''}`}>
       <div className='modal__background'></div>
       <div className='modal__container' onClick={onWindowClose}>
-        <div
-          className='modal__content'
-          onClick={(event) => {
-            event.stopPropagation();
-          }}
-        >
+        <div className='modal__content' onClick={stopPropagation}>
           <div className='modal__btns'>
-            <div className='modal__close'>
-              <button
-                className='modal__btn-close'
-                onClick={onWindowClose}
-                title='Close window'
-              >
-                <span>Close window</span>
-              </button>
-            </div>
+            <div className='modal__close'>{button}</div>
           </div>
           <div className='modal__body'>{children}</div>
         </div>
