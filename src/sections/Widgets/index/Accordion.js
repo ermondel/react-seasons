39a0eeae930323061a742b574fa66eb7da27b711
ermondel <ermondel@gmail.com>
2020-08-08 21:31:58 +0300

    feat: add accordion widget

--- src/sections/Widgets/index/Accordion.js
+++ src/sections/Widgets/index/Accordion.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from '../sources/accordionSource';
 
-const Accordion = () => <p>Accordion</p>;
+const Accordion = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  return (
+    <dl className='widgets-accordion'>
+      {data.map((item) => {
+        const isActive = item.id === activeIndex;
+
+        const dtClass = isActive
+          ? 'widgets-accordion__term--active'
+          : 'widgets-accordion__term';
+        const ddClass = isActive
+          ? 'widgets-accordion__definition--active'
+          : 'widgets-accordion__definition';
+        const activate = () => setActiveIndex(isActive ? null : item.id);
+
+        return (
+          <React.Fragment key={item.id}>
+            <dt
+              onClick={activate}
+              onKeyPress={activate}
+              className={dtClass}
+              tabIndex={0}
+            >
+              <div className='widgets-accordion__title'>{item.title}</div>
+            </dt>
+            <dd className={ddClass}>{item.content}</dd>
+          </React.Fragment>
+        );
+      })}
+    </dl>
+  );
+};
 
 export default Accordion;
---
+++ src/sections/Widgets/sources/accordionSource.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 'da264c57-be15-4eab-bca4-968a2acddfcd',
+    title: 'Physics',
+    content:
+      'Physics is the natural science that studies matter, its motion and behavior through space and time, and the related entities of energy and force. Physics is one of the most fundamental scientific disciplines, and its main goal is to understand how the universe behaves.',
+  },
+  {
+    id: 'fc2175c3-5ff2-4242-81ce-a82713835288',
+    title: 'Chemistry',
+    content:
+      'Chemistry is the scientific discipline involved with elements and compounds composed of atoms, molecules and ions: their composition, structure, properties, behavior and the changes they undergo during a reaction with other substances.',
+  },
+  {
+    id: '0c949b52-0bb0-4724-9982-ff034470c17f',
+    title: 'Biology',
+    content:
+      'Biology is the natural science that studies life and living organisms, including their physical structure, chemical processes, molecular interactions, physiological mechanisms, development and evolution.',
+  },
+  {
+    id: '774e93fe-39fb-4676-b482-4d06e1f42b2c',
+    title: 'Astronomy',
+    content:
+      'Astronomy is a natural science that studies celestial objects and phenomena. It uses mathematics, physics, and chemistry in order to explain their origin and evolution. Objects of interest include planets, moons, stars, nebulae, galaxies, and comets.',
+  },
+  {
+    id: 'f8a15428-eb1e-4b25-a290-28d125cfe495',
+    title: 'History',
+    content:
+      'History is the study of the past. Events occurring before the invention of writing systems are considered prehistory. "History" is an umbrella term that relates to past events as well as the memory, discovery, collection, organization, presentation, and interpretation of information about these events.',
+  },
+];
