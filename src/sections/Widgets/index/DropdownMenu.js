76d66fb3b923e8c48290ef6c464595c4a52e6322
ermondel <ermondel@gmail.com>
2020-08-15 22:11:32 +0300

    feat: add style to the dropdown menu widget

--- src/sections/Widgets/index/DropdownMenu.js
+++ src/sections/Widgets/index/DropdownMenu.js
@@ -5,34 +5,30 @@ const DropdownMenu = ({ options, selected, onSelectedChange }) => {
   const ref = useRef();
 
   useEffect(() => {
-    const onBodyClick = (event) => {
-      if (!ref.current.contains(event.target)) {
-        setOpen(false);
-      }
-    };
+    const onBodyClick = (e) => !ref.current.contains(e.target) && setOpen(false);
 
     document.body.addEventListener('click', onBodyClick);
 
-    return () => {
-      document.body.removeEventListener('click', onBodyClick);
-    };
+    return () => document.body.removeEventListener('click', onBodyClick);
   }, []);
 
   if (!selected) selected = options[0];
 
   const renderOption = (option) => {
-    return selected.value === option.value ? (
-      <li key={option.value} className='dropdown-menu__option--active'>
-        {option.label}
-      </li>
-    ) : (
+    const isActive = selected.value === option.value;
+
+    const onOptionClick = () => {
+      setOpen(false);
+      if (!isActive) onSelectedChange(option);
+    };
+
+    return (
       <li
         key={option.value}
-        className='dropdown-menu__option'
-        onClick={() => {
-          setOpen(false);
-          onSelectedChange(option);
-        }}
+        className={'dropdown-menu__option' + (isActive ? '--active' : '')}
+        onClick={onOptionClick}
+        onKeyPress={onOptionClick}
+        tabIndex={0}
       >
         {option.label}
       </li>
@@ -42,12 +38,14 @@ const DropdownMenu = ({ options, selected, onSelectedChange }) => {
   return (
     <div className='dropdown-menu' ref={ref}>
       <div className='dropdown-menu__label'>Select option</div>
-      <div className='dropdown-menu__selected' onClick={() => setOpen(!open)}>
-        {selected.label}
+      <div className='dropdown-menu__element'>
+        <button className='dropdown-menu__btn' onClick={() => setOpen(!open)}>
+          {selected.label}
+        </button>
+        {open ? (
+          <ul className='dropdown-menu__options'>{options.map(renderOption)}</ul>
+        ) : null}
       </div>
-      {open ? (
-        <ul className='dropdown-menu__options'>{options.map(renderOption)}</ul>
-      ) : null}
     </div>
   );
 };
